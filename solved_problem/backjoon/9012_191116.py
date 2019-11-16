8cbd7adcb216be8679aa0c6c29befe9af933edd2
AgarYoBob <32961681+AgarYoBob@users.noreply.github.com>
2019-11-16 14:22:42 +0900

    괄호

--- solved_problem/backjoon/9012_191116.py
+++ solved_problem/backjoon/9012_191116.py
@@ -4,15 +4,16 @@ for t in range (int(input())):
 	a = input()
 	b = []
 	for c in a:
-		#print(c)
 		if c == '(':
 			b.append(1)
 		else:
-			if len(b) == 0:
-				b.append(2)
+			try:
+				b.pop()
+			except IndexError:
+				b = "IndexError"
+				print("NO")
 				break
-			b.pop()
 	if len(b) == 0:
 		print("YES")
-	else:
+	elif b is not "IndexError":
 		print("NO")
