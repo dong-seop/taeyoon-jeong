40d8955127ea3a97a9008c6e6d57174b2914d8e7
AgarYoBob <32961681+AgarYoBob@users.noreply.github.com>
2019-11-30 14:33:51 +0900

    큐

--- solved_problem/backjoon/10845_191123(시간초과).py
+++ solved_problem/backjoon/10845_191123(시간초과).py
@@ -1,40 +1,47 @@
 # https://www.acmicpc.net/problem/10845
 
-#import sys
-# n = sys.stdin.readline()
-# cmds = sys.stdin.readlines()
+import sys
 
+n = sys.stdin.readline()
+cmds = sys.stdin.readlines()
 a = []
-for i in range(int(input())):
-	b = list(input().split())
-	
-	if b[0] == 'push':
-		a.append(b[1])
+size = 0
 
-	elif b[0] == 'pop':
+for cmd in cmds:
+	cmd = cmd.rstrip()
+	#print(a)
+
+	if cmd == 'pop':
 		try:
 			print(a[0])
 			del a[0]
+			size -= 1
 		except IndexError:
 			print(-1)
 
-	elif b[0] == 'size':
-		print(len(a))
+	elif cmd == 'size':
+		print(size)
 
-	elif b[0] == 'empty':
-		if len(a) == 0:
-			print(1)
-		else:
+	elif cmd == 'empty':
+		if size:
 			print(0)
+		else:
+			print(1)
 
-	elif b[0] == 'front':
+	elif cmd == 'front':
 		try:
 			print(a[0])
 		except IndexError:
 			print(-1)
 
-	elif b[0] == 'back':
+	elif cmd == 'back':
 		try:
 			print(a[len(a) - 1])
 		except IndexError:
 			print(-1)
+
+	else:
+		#print(cmd)
+		a.append(int(cmd.split()[1]))
+		size+=1
+		
