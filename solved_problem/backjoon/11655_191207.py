9e9ef19704c55c48871b5029805110960897b237
AgarYoBob <32961681+AgarYoBob@users.noreply.github.com>
2019-12-07 15:08:06 +0900

    ROT13

---
+++ solved_problem/backjoon/11655_191207.py
@@ -0,0 +1,15 @@
+https://www.acmicpc.net/problem/11655
+
+a = input()
+a = list(a)
+for i in range (len(a)):
+	a[i] = ord(a[i])
+for i in range (len(a)):
+	if 65 <= a[i] <= 90 or 97 <= a[i] <= 122:
+		a[i] += 13
+		if 91 <= a[i] <= 104 or 123 <= a[i]:
+			a[i] -= 26
+for i in range (len(a)):
+	a[i] = chr(a[i])
+	print(a[i], end='')
+  
