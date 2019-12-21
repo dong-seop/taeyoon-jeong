22558cf45c81604d8623e2d9c11d6c3bb814819c
AgarYoBob <32961681+AgarYoBob@users.noreply.github.com>
2019-12-21 14:10:59 +0900

    1로 만들기

--- solved_problem/backjoon/1463_191207.py
+++ solved_problem/backjoon/1463_191207.py
@@ -1,9 +1,19 @@
 # https://www.acmicpc.net/problem/11656
 
-x = int(input())
-a = [1]
+X = int(input())
+dp = [X]
+cnt = 0
 
-while (1):
-	a = list(set(a))
-	print(a)
-  
+while True:
+    if 1 in dp:
+        print(cnt)
+        break
+    calc = []
+    for i in dp:
+        if i % 2 == 0:
+            calc.append(int(i / 2))
+        if i % 3 == 0:
+            calc.append(int(i / 3))
+        calc.append(i - 1)
+    dp = calc
+    cnt += 1
