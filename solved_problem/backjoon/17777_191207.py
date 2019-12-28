02956139e02cb465a79d1970f0ed77ed8dc031f1
AgarYoBob <32961681+AgarYoBob@users.noreply.github.com>
2019-12-28 16:30:08 +0900

    Copy and Paste (시간초과, 문제 한국어 아님.)

--- solved_problem/backjoon/17777_191207.py
+++ solved_problem/backjoon/17777_191207.py
@@ -1,15 +1,19 @@
 # https://www.acmicpc.net/problem/17777
 
-m = int(input())
-s = list(input())
-n = int(input())
-
 import sys
 
+m = int(sys.stdin.readline())
+s = list(sys.stdin.readline())
+n = int(sys.stdin.readline())
+
+
+a = sys.stdin.readlines()
 for i in range (n):
-	a = map(int, sys.stdin.readline())
-	temp = s[a[0]:a[1]]
-	s.insert(a[2], temp)
+	a[i] = a[i].split()
+
+for i in range (n):
+	temp = s[int(a[i][0]):int(a[i][1])]
+	s.insert(int(a[i][2]), temp)
 	s = [y for x in s for y in x]
 	if len(s) > m:
 		s = s[0:m]
